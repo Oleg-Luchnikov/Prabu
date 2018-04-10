ef7a489d97450a70461cdeaff70abb3a0208559d
Oleg <oleg19931208@gmail.com>
2018-04-11 00:38:09 +0300

    add modal window

--- js/main.js
+++ js/main.js
@@ -13,16 +13,45 @@ $(document).ready(function() {
 		}
 		return false;
 	});
-});
+	 $("#mobile-menu").mmenu({
+     // options
+     navbar: {
+     	add: false
+     }
+  }, {
+     // configuration
+     offCanvas: {
+        pageSelector: ".wrapper"
+     },
+      classNames: {
+          fixedElements: {
+             sticky: "header-navbar",
+             elemInsertMethod: "prependTo",
+          }
+       }
 
-$(document).ready(function(){
-	
-	// Variables
+
+  });
+  var API = $("#mobile-menu").data( "mmenu" );
+	  
+  $("#hamburger-button").click(function() {
+     API.open();
+     return false;
+  });
+  $("#hamburger-button").click(function() {
+     API.close();
+  });
+  $(".mm-listitem .link-item").click(function() {
+     API.close();
+  });
+
+  // Variables
 	var clickedTab = $(".tabs > .active");
 	var tabWrapper = $(".tab__content");
+	var navWrapper = $(".tabs-nav");
 	var activeTab = tabWrapper.find(".active");
 	var activeTabHeight = activeTab.outerHeight();
-	
+	var activeNav = navWrapper.find(".active");
 	// Show tab on page load
 	activeTab.show();
 	
@@ -33,13 +62,19 @@ $(document).ready(function(){
 		
 		// Remove class from active tab
 		$(".tabs > li").removeClass("active");
-		
+		$(".tabs-nav > li").removeClass("active");
 		// Add class active to clicked tab
 		$(this).addClass("active");
-		
+
+
+
 		// Update clickedTab variable
 		clickedTab = $(".tabs .active");
-		
+
+		var clickedTabIndex = clickedTab.index();
+
+		$(".tabs-nav > li").eq(clickedTabIndex).addClass("active");
+		activeNav = $(".tabs-nav > li")
 		// fade out active tab
 		activeTab.fadeOut(250, function() {
 			
@@ -59,17 +94,100 @@ $(document).ready(function(){
 			activeTabHeight = activeTab.outerHeight();
 			
 			// Animate height of wrapper to new tab height
-			tabWrapper.stop().delay(50).animate({
+			tabWrapper.stop().delay(10).animate({
 				height: activeTabHeight
 			}, 500, function() {
 				
 				// Fade in active tab
-				activeTab.delay(50).fadeIn(250);
+				activeTab.delay(10).fadeIn(250);
 				
 			});
 		});
 	});
+	$(".tabs-nav > li").on("click", function() {
+		
+		// Remove class from active tab
+		$(".tabs > li").removeClass("active");
+		$(".tabs-nav > li").removeClass("active");
+		// Add class active to clicked tab
+		$(this).addClass("active");
+
+
+
+		// Update clickedTab variable
+		clickedTab = $(".tabs-nav .active");
+
+		var clickedTabIndex = clickedTab.index();
+		$(".tabs > li").eq(clickedTabIndex).addClass("active");
+		clickedTab = $(".tabs .active");
+		// fade out active tab
+		activeTab.fadeOut(250, function() {
+			
+			// Remove active class all tabs
+			$(".tab__content > li").removeClass("active");
+			
+			// Get index of clicked tab
+			var clickedTabIndex = clickedTab.index();
+
+			// Add class active to corresponding tab
+			$(".tab__content > li").eq(clickedTabIndex).addClass("active");
+			
+			// update new active tab
+			activeTab = $(".tab__content > .active");
+			
+			// Update variable
+			activeTabHeight = activeTab.outerHeight();
+			
+			// Animate height of wrapper to new tab height
+			tabWrapper.stop().delay(10).animate({
+				height: activeTabHeight
+			}, 500, function() {
+				
+				// Fade in active tab
+				activeTab.delay(10).fadeIn(250);
+				
+			});
+		});
+	});
+
+	$(".open-all").click(function() {
+		$(".accordeon_item").addClass('active');
+		$('.accordeon_content').slideDown();
+		return false;
+	});
+
+	$('.modal-btn').magnificPopup({
+	    type: 'inline',
+	    preloader: false,
+	    focus: '#name',
+	    fixedContentPos: true,
+	    fixedBgPos: true,
+	    overflowY: 'auto',
+
+	    // When elemened is focused, some mobile browsers in some cases zoom in
+	    // It looks not nice, so we disable it:
+	    callbacks: {
+	      beforeOpen: function() {
+	        
+	        if($(window).width() < 700) {
+	          this.st.focus = false;
+
+	        } else {
+	          this.st.focus = '#name';
+	        }
+	      },
+	      open: function(){
+	        if ( $('.mfp-content').height() < $(window).height() ){
+	          $('body').on('touchmove', function (e) {
+	              e.preventDefault();
+	          });
+	        }
+	      },
+	    }
+	});
+
 });
+
 
 $(function(){
   // hide all content
@@ -83,10 +201,4 @@ $(function(){
 			$(this).next().slideDown();
 		}
   });
-});
-
-$(".open-all").click(function() {
-	$(".accordeon_item").addClass('active');
-	$('.accordeon_content').slideDown();
-	return false;
 });
